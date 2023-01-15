f293c18ef1b5b0cb2649a18c37363f84b824196f
Haya Taqatqa <100483404+hayataq@users.noreply.github.com>
2023-01-15 12:38:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const mongoose = require("mongoose");
 const cors = require('cors')
 const app = express()
 app.use(cors())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 //
 
 const coursesRoute = require("./routes/courses");
