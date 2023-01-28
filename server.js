d6b99726e5fc37bf14e42b2a068c8ef903eccfb5
AmirAmArA <amiramara11@gmail.com>
2023-01-28 16:36:42 +0200

    added submission

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const coursesRoute = require("./routes/courses");
 const studentsRoute = require("./routes/students");
 const assignmentRoute = require("./routes/assignments");
 const userRoute = require("./routes/users");
-// const submissionRouter = require("./routes/submissions");
+const submissionRouter = require("./routes/submissions");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 app.use("/api/courses", coursesRoute);
 app.use("/api/students", studentsRoute);
 app.use("/api/assignment", assignmentRoute);
-// app.use("/api/submission", submissionRouter);
+app.use("/api/submission", submissionRouter);
 app.use("/api/user", userRoute);
 
 mongoose.set("strictQuery", false);
